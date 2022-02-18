77ad44b4415f7b474d345e2c5f8b2492e32c1dad
Kir Proto Q <kir.proto@ya.ru>
2022-02-18 11:51:39 +0500

    Ready missing Flag

--- Yan_SwiftUI_Maps_SwiftUI/ContentView.swift
+++ Yan_SwiftUI_Maps_SwiftUI/ContentView.swift
@@ -6,13 +6,65 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        MapView()
+            .edgesIgnoringSafeArea(.all)
     }
 }
+
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        //        Add point on the Map
+        let annotation = MKPointAnnotation()
+        annotation.title = "Sochi"
+        annotation.subtitle = "Hometown"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 43, longitude: 43)
+        mapView.addAnnotation(annotation)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        //
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        
+        var parent: MapView
+        
+        func mapViewDidChangeVisibleRegion (_ mapView: MKMapView) {
+            
+            print(mapView.centerCoordinate)
+            
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+    }
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
