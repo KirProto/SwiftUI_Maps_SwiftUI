42272789609fc314383c594bfe96920dcb2fe300
Kir Proto Q <kir.proto@ya.ru>
2022-02-18 11:18:54 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Maps_SwiftUI/Yan_SwiftUI_Maps_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Maps_SwiftUIApp.swift
+//  Yan_SwiftUI_Maps_SwiftUI
+//
+//  Created by KIR Q on 2022.02.18.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Maps_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
